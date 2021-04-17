a4ec80021496f245acc159f52f04fe54b8ee550e
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-17 20:04:19 +0200

    Created Footer Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from'./Navbar';
-import Header from './Header'
+import Header from './Header';
+import Footer from './Footer'
 
 class App extends Component {
   render() {
@@ -78,41 +79,9 @@ class App extends Component {
 
         <hr/>
 
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-md-10 mx-auto">
-                <ul className="list-inline text-center">
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <span className="fa-stack fa-lg">
-                        <i className="fas fa-circle fa-stack-2x"></i>
-                        <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-                <p className="copyright text-muted">Copyright &copy; Your Website 2020</p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
+
+        
       </div>
     );
   }
